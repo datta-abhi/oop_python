56791e8cf6df56f07f8bba5c44ee073c092099d0
datta-abhi <rookbee91@gmail.com>
2024-02-13 23:45:06 +0530

    class method added

--- instance_method.py
+++ instance_method.py
@@ -12,7 +12,18 @@ class Person:
     def name_length(self):
         return len(self.name)
 
+    # class method to calculate age from birth year
+    @classmethod
+    def from_birth_year(cls,name,birth_year):
+        age = 2024 - birth_year
+        return cls(name,age)
+
+
 p1 = Person('Abhi',32)
 p1.greet()
 length = p1.name_length()
 print(length)
+print('-'*30)
+
+p2 = Person.from_birth_year('Bobby',1997)
+p2.greet()
