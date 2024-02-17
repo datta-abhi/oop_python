a539f8ca962bded4f5ee9439b6afd0a9a2eee805
datta-abhi <rookbee91@gmail.com>
2024-02-17 12:34:41 +0530

    edit functionality added to student management

--- student_management.py
+++ student_management.py
@@ -21,12 +21,19 @@ class School:
             print('Roll Number: ', student.roll_number)
             print('-'*30)
 
+    def edit_student(self,roll_number,new_name,new_age):
+        for s in self.students:
+            if s.roll_number == roll_number:
+                s.name = new_name
+                s.age = new_age
+                print('Student will roll number {} has been edited'.format(s.roll_number))
+                return
 # Creating school Object
 school = School()
 
 while True:
     # take user input of student info one at a time
-    choice = input('Enter your choice \n1) Add Student \n2) Display List of students \n3) Quit: \n')
+    choice = input('Enter your choice \n1) Add Student \n2) Display List of students \n3) Edit data \n5) Quit: \n')
 
     if choice =='1':
         name = input('Enter name: ')
@@ -38,6 +45,12 @@ while True:
 
     elif choice =='2':
         school.display_students()
+
+    elif choice =='3':
+        roll_num = input('Enter roll number of student yu want to edit: ')
+        new_name = input('Enter new name: ')
+        new_age = input('Enter new age: ')
+        school.edit_student(roll_num,new_name,new_age)
     else:
         break
 
